22385dbe027bee2d21d75b811bc9052fb477ae17
jhanoo15 <jhanoo15@gmail.com>
2025-12-29 15:20:27 +0900

    [init] 프로젝트 init

---
+++ src/main/kotlin/com/sixclassguys/maplecalendar/MapleCalendarBackEndApplication.kt
@@ -0,0 +1,11 @@
+package com.sixclassguys.maplecalendar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MapleCalendarBackEndApplication
+
+fun main(args: Array<String>) {
+    runApplication<MapleCalendarBackEndApplication>(*args)
+}
---
+++ src/test/kotlin/com/sixclassguys/maplecalendar/MapleCalendarBackEndApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sixclassguys.maplecalendar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MapleCalendarBackEndApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
