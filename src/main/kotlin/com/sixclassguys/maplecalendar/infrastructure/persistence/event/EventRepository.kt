a2c101d7403cb1d33eb83dadbeaab2e9836b5584
littlesam95 <littlesam95@naver.com>
2026-01-12 00:18:59 +0900

    [feat] DB에 저장된, 오늘 진행중인 이벤트 조회

--- src/main/kotlin/com/sixclassguys/maplecalendar/infrastructure/persistence/event/EventRepository.kt
+++ src/main/kotlin/com/sixclassguys/maplecalendar/infrastructure/persistence/event/EventRepository.kt
@@ -9,6 +9,9 @@ import java.time.LocalDateTime
 @Repository
 interface EventRepository : JpaRepository<Event, Long> {
 
+    @Query("SELECT e FROM Event e WHERE e.startDate <= :today AND e.endDate >= :today")
+    fun getEventsForToday(today: LocalDateTime): List<Event>
+
     // endDate가 파라미터로 넘어온 날짜와 일치하는 이벤트 리스트 조회
     @Query("SELECT e FROM Event e WHERE e.endDate >= :start AND e.endDate <= :end")
     fun findAllByEndDateBetween(start: LocalDateTime, end: LocalDateTime): List<Event>
