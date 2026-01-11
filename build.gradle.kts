f0ecf91aa63a3b859aad53e09235900a8746b5f1
littlesam95 <littlesam95@naver.com>
2026-01-12 00:19:40 +0900

    [chore] 의존성 추가, gitkeep 파일 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 }
 
 kotlin {
